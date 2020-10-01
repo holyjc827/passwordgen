52a5e73d0eb7dd497f9c0a81ff28b5978b76b1ef
holyjc827 <holyjc827@gmail.com>
2020-10-01 21:49:07 +0200

    Password generating feature has been created: 1) The user will be asked to type how long the password should be 2) The user will be asked if special characters should be included in the password

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Hello World!")
-print("Chu")
+import random, string
+
+def generate():
+	rand = random.SystemRandom()
+	length = int(input("how many digits do you want your password to be? "))
+	specialChar = input("Do you want one or more special characters in your password? (Y/N) ")
+	if specialChar == "Y":
+		alphabet = string.ascii_letters + string.digits + string.punctuation
+		password = str().join(rand.choice(alphabet) for _ in range(length))
+	elif specialChar == "N":
+		alphabet = string.ascii_letters + string.digits
+		password = str().join(rand.choice(alphabet) for _ in range(length))
+	else:
+		print("Please type either Y or N (case-sensitive).")
+	return password
+
+password = generate()
+print(password)
